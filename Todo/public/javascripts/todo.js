b53ac369a930ff45a08681ad7f81f53ac5e6bfa0
kb4649 <kb4649web@gmail.com>
2018-08-22 15:01:51 +0900

    all functions

--- Todo/public/javascripts/todo.js
+++ Todo/public/javascripts/todo.js
@@ -1,4 +1,4 @@
-var Items=[];
+var Items=[]; //配列に保存。リロードすると消える
 
 document.getElementById('add').onclick =function(){
 	var str0=document.getElementById('text').value; //入力された値
@@ -8,11 +8,21 @@ document.getElementById('add').onclick =function(){
 	list();
 }
 
+function del(id){
+	if((id<0)||(id>=Items.length)) return;//範囲外
+	console.log('del ('+id+')');
+	for(i0=id;i0<(Items.length-1);i0++){
+		Items[id]=Items[id+1];//１つずつ詰める
+	}
+	Items.pop();
+	list();
+}
+
 function list(){
 	var str0='';
 	for(i0=(Items.length-1);i0>=0;i0--){//逆順
-		str0+='<li>'+Items[i0]+'</li>';
+		str0+='<li><input type="submit" onclick="del('+i0
+		+')" value="削除" />'+Items[i0]+'</li>';
 	}
 	document.getElementById('list').innerHTML=str0;
 }
-
