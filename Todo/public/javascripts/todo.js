e53d55d1a1f1cdf41c5e38d71b5fb29befc8459b
kb4649 <kb4649web@gmail.com>
2018-08-23 11:26:33 +0900

    table styele, DEL index bugfix

--- Todo/public/javascripts/todo.js
+++ Todo/public/javascripts/todo.js
@@ -12,7 +12,7 @@ function del(id){
 	if((id<0)||(id>=Items.length)) return;//範囲外
 	console.log('del ('+id+')');
 	for(i0=id;i0<(Items.length-1);i0++){
-		Items[id]=Items[id+1];//１つずつ詰める
+		Items[i0]=Items[i0+1];//１つずつ詰める
 	}
 	Items.pop();
 	list();
@@ -20,11 +20,12 @@ function del(id){
 
 function list(){
 	var str0='<table>'
-	+'<thead><tr><th style="width:80%">　内容　</th><th>削除</th></tr></thead>'
+	+'<thead><tr><th>内容</th>'
+	+'<th class="th-btn">削除</th></tr></thead>'
 	+'<tbody>';
 	for(i0=(Items.length-1);i0>=0;i0--){//逆順
-		str0+='<tr><td>'+Items[i0]+'</td>'
-		+'<td><input type="submit" onclick="del('+i0
+		str0+='<tr><td>'+Items[i0]+'</td><td class="td-btn">'
+		+'<input type="submit" onclick="del('+i0
 		+')" value="削除" /></td></tr>';
 	}　　
 	　str0+='</tbody></table>';
