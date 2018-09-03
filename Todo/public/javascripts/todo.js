8cb3e1cc91c3c683d5774f94d0395a403058114b
kb4649 <kb4649web@gmail.com>
2018-09-03 16:25:17 +0900

    Post API add, del

--- Todo/public/javascripts/todo.js
+++ Todo/public/javascripts/todo.js
@@ -1,5 +1,3 @@
-//var Items=[]; //配列に保存。リロードすると消える
-
 document.getElementById('add').onclick =function(){
 	var str0=document.getElementById('text').value; //入力された値
 	if(str0=='')return;//文字列なし
@@ -8,17 +6,17 @@ document.getElementById('add').onclick =function(){
 	xhr.open("POST","/add",true);
 	xhr.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
 	xhr.send('text='+str0);
-//	Items.push(str0);
 	list();
 }
 
 function del(id){
-//	if((id<0)||(id>=Items.length)) return;//範囲外
-	console.log('del ('+id+')');
-//	for(i0=id;i0<(Items.length-1);i0++){
-//		Items[i0]=Items[i0+1];//１つずつ詰める
-//	}
-//	Items.pop();
+	var str0=id; //文字列
+	if(str0=='')return;//文字列なし
+	console.log('del '+str0);
+	var xhr=new XMLHttpRequest();
+	xhr.open("POST","/del",true);
+	xhr.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
+	xhr.send('id='+str0);
 	list();
 }
 
@@ -34,8 +32,8 @@ function list(){
 		+'<tbody>';
 		for(i0=(Items.length-1);i0>=0;i0--){//逆順
 			str0+='<tr><td>'+Items[i0].text+'</td><td class="td-btn">'
-			+'<input type="submit" onclick="del('+i0
-			+')" value="削除" /></td></tr>';
+			+'<input type="submit" onclick="del(\''+Items[i0]._id
+			+'\')" value="削除" /></td></tr>';
 		}　　
 		 str0+='</tbody></table>';
 		document.getElementById('list').innerHTML=str0;
